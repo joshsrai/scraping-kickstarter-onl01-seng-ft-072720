c28cfc244d7bd5406d4024eb95dff1be0c5dbf25
Josh Raiborde <joshsrai@yahoo.com>
2020-08-15 23:25:33 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,8 @@ require "pry"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-#  kickstarter.css("li.project.grid_4").first
+  kickstarter.css("li.project.grid_4").first
+  project = _ 
 project.css("h2.bbcard_name strong a").text
   binding.pry
 end
